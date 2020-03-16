306fae4f9a2eeee68540aa60b19aacd6a08c5df9
Ivan Novichenka <i.novichenka@godeltech.com>
2020-03-16 11:32:50 +0300

    fix

--- .teamcity/Build.kts
+++ .teamcity/Build.kts
@@ -1,3 +1,11 @@
+import jetbrains.buildServer.configs.kotlin.v2018_2.*
+import jetbrains.buildServer.configs.kotlin.v2018_2.buildFeatures.Swabra
+import jetbrains.buildServer.configs.kotlin.v2018_2.buildFeatures.swabra
+import jetbrains.buildServer.configs.kotlin.v2018_2.buildSteps.maven
+import jetbrains.buildServer.configs.kotlin.v2018_2.triggers.vcs
+import jetbrains.buildServer.configs.kotlin.v2018_2.vcs.GitVcsRoot
+
+
 object Build : BuildType({
     name = "Build"
     artifactRules = "target/*jar"
