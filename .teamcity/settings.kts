fa610a00810749a94d4ccf53268005d5c785a248
Ivan Novichenka <i.novichenka@godeltech.com>
2020-03-17 13:09:15 +0300

    fix

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -21,13 +21,10 @@ To debug in IntelliJ Idea, open the 'Maven Projects' tool window (View
 (Plugins -> teamcity-configs -> teamcity-configs:generate), the
 'Debug' option is available in the context menu for the task.
 */
+@file:Include("./buildsteps/Build.kts")
 
 version = "2019.2"
 
 project {
     buildType(Build)
 }
-
-object Build : BuildType({
-
-})
